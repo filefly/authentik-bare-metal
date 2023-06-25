2d6c5025d6c9b2e8814aebba26c5fdd508a52121
Brennan Godfrey <godfreybr@users.noreply.github.com>
2023-06-25 15:41:48 +0800

    Update 03_clone.sh
    Updated poetry export to change "--dev" to "--with dev" due to deprecation.
    Added static git clone specific version

--- 03_clone.sh
+++ 03_clone.sh
@@ -1,15 +1,26 @@
+#!/bin/bash
+
+# Specify version to clone instead of "master"
+authentik_version="version/2023.5.4"
+
 cd /opt/authentik
+
+# Remove pre-existing git clones
 rm -rf ./src
-git clone https://github.com/goauthentik/authentik.git src
+
+# Clone specific version of Authentik source
+git clone --depth 1 --branch version/2023.5.4 https://github.com/goauthentik/authentik.git src
 
 cd /opt/authentik/src/
 
+# Create Python virtual environment
 python3.11 -m virtualenv ./.venv
 source .venv/bin/activate
 
-# Install dependencies
+# Create export of PIP compatible dependencies
 pip install --no-cache-dir poetry
 poetry export -f requirements.txt --output requirements.txt
-poetry export -f requirements.txt --dev --output requirements-dev.txt
+poetry export -f requirements.txt --with dev --output requirements-dev.txt
 
+# Install dependencies
 pip install --no-cache-dir -r requirements.txt -r requirements-dev.txt
