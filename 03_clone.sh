55366a324d4aec3952a8029f557aba0b02e05aca
Brennan Godfrey <godfreybr@users.noreply.github.com>
2023-06-25 15:44:12 +0800

    Added note of error to investigate

--- 03_clone.sh
+++ 03_clone.sh
@@ -23,4 +23,8 @@ poetry export -f requirements.txt --output requirements.txt
 poetry export -f requirements.txt --with dev --output requirements-dev.txt
 
 # Install dependencies
+# Error to investigate
+# ERROR: pip's dependency resolver does not currently take into account all the packages that are installed. This behaviour is the source of the following dependency conflicts.
+# virtualenv 20.23.1 requires platformdirs<4,>=3.5.1, but you have platformdirs 2.5.4 which is incompatible.
+# poetry 1.5.1 requires platformdirs<4.0.0,>=3.0.0, but you have platformdirs 2.5.4 which is incompatible.
 pip install --no-cache-dir -r requirements.txt -r requirements-dev.txt
