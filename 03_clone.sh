d9df4123803832e89d51bbb2e972b7915469f105
Filefly <filefly@gmail.com>
2024-06-02 00:27:00 -0600

    Install authentik from master branch

--- 03_clone.sh
+++ 03_clone.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
-# Specify version to clone instead of "master"
-authentik_version="version/2023.5.4"
-
 cd /opt/authentik
 
 # Remove pre-existing git clones
 rm -rf ./src
 
 # Clone specific version of Authentik source
-git clone --depth 1 --branch version/2023.5.4 https://github.com/goauthentik/authentik.git src
+git clone --depth 1 https://github.com/goauthentik/authentik.git src
 
 cd /opt/authentik/src/
 
