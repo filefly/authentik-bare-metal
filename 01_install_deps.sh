a14cda96571d57e20d726a5b313d85ccce25d231
Filefly <filefly@gmail.com>
2024-06-02 08:41:47 -0600

    Remove dependency on sudo since already running as root

--- 01_install_deps.sh
+++ 01_install_deps.sh
@@ -1,7 +1,7 @@
 STARTING_DIR=$(pwd)
 
-sudo apt update && sudo apt upgrade -yqq # Ensure up-to-date system
-sudo apt install -yqq curl wget git build-essential libncursesw5-dev libssl-dev \
+apt update && sudo apt upgrade -yqq # Ensure up-to-date system
+apt install -yqq curl wget git build-essential libncursesw5-dev libssl-dev \
      libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev pkg-config libffi-dev zlib1g-dev libxmlsec1 libxmlsec1-dev libxmlsec1-openssl libmaxminddb0 # Install build dependencies
 
 wget https://github.com/mikefarah/yq/releases/download/v4.30.8/yq_linux_amd64 -O /usr/bin/yq
@@ -22,5 +22,5 @@ apt install -y nodejs
 
 # Install Go 1.22
 wget https://go.dev/dl/go1.22.3.linux-amd64.tar.gz
-sudo tar -C /usr/local -xzf go1.22.3.linux-amd64.tar.gz
+tar -C /usr/local -xzf go1.22.3.linux-amd64.tar.gz
 rm -rf go1.22.3.linux-amd64.tar.gz
