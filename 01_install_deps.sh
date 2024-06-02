0d76647bf7c9fe297e414bfdd7994e5bee50aa2f
Filefly <filefly@gmail.com>
2024-06-02 08:42:11 -0600

    Remove dependency on sudo since already running as root

--- 01_install_deps.sh
+++ 01_install_deps.sh
@@ -1,6 +1,6 @@
 STARTING_DIR=$(pwd)
 
-apt update && sudo apt upgrade -yqq # Ensure up-to-date system
+apt update && apt upgrade -yqq # Ensure up-to-date system
 apt install -yqq curl wget git build-essential libncursesw5-dev libssl-dev \
      libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev pkg-config libffi-dev zlib1g-dev libxmlsec1 libxmlsec1-dev libxmlsec1-openssl libmaxminddb0 # Install build dependencies
 
